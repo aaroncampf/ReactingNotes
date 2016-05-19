3f330879d55dd72c60c28402be07110e6fa183ef
Aaron Campf <aaroncampf@gmail.com>
2016-05-19 12:36:19 -0700

    More Work

--- ReactingNotes/ReactingNotes/Scripts/HelloReact.js
+++ ReactingNotes/ReactingNotes/Scripts/HelloReact.js
@@ -22,6 +22,38 @@ var QuoteLine = (function () {
     }
     return QuoteLine;
 })();
+var Contact = (function () {
+    function Contact() {
+    }
+    return Contact;
+})();
+var Note = (function () {
+    function Note() {
+    }
+    return Note;
+})();
+var ContactDisplay = (function (_super) {
+    __extends(ContactDisplay, _super);
+    function ContactDisplay() {
+        var _this = this;
+        _super.apply(this, arguments);
+        this.ViewNotes = function (Current) {
+            $("#ContactDisplay-" + _this.props.ID).append("<div id='fhhgk75d472' />");
+            var Rows = [];
+            for (var _i = 0, _a = _this.props.Notes; _i < _a.length; _i++) {
+                var Item = _a[_i];
+                var Test = React.createElement("tr", null, React.createElement("td", null, Item.ID), React.createElement("td", null, Item.Date), React.createElement("td", null, Item.Title), React.createElement("td", null, Item.Text));
+                Rows.push(Test);
+            }
+            return (React.createElement("table", {"id": "QuoteDisplay-" + _this.props.ID, "className": "table-striped table-bordered"}, React.createElement("tbody", null, React.createElement("tr", null, React.createElement("td", {"colSpan": 5, "style": { "text-align": "center" }}, React.createElement("h3", null, _this.props.Name))), Rows)));
+        };
+    }
+    ContactDisplay.prototype.render = function () {
+        var _this = this;
+        return (React.createElement("table", {"id": "ContactDisplay-" + this.props.ID, "className": "table-striped table-bordered"}, React.createElement("tbody", null, React.createElement("tr", null, React.createElement("td", {"colSpan": 2, "style": { "text-align": "center" }}, React.createElement("h3", null, this.props.Name))), React.createElement("tr", null, React.createElement("td", null, "ID"), React.createElement("td", null, this.props.ID)), React.createElement("tr", null, React.createElement("td", null, "Phone"), React.createElement("td", null, this.props.Phone)), React.createElement("tr", null, React.createElement("td", null, "Email"), React.createElement("td", null, this.props.Email)), React.createElement("tr", null, React.createElement("td", null, "Position"), React.createElement("td", null, this.props.Position)), React.createElement("tr", null, React.createElement("td", {"colSpan": 2}, React.createElement("a", {"className": "btn btn-primary", "onClick": function () { return _this.ViewNotes(_this); }}, "View Contacts"))))));
+    };
+    return ContactDisplay;
+})(React.Component);
 var QuoteDisplay = (function (_super) {
     __extends(QuoteDisplay, _super);
     function QuoteDisplay() {
@@ -34,7 +66,7 @@ var QuoteDisplay = (function (_super) {
             var Test = React.createElement("tr", null, React.createElement("td", null, Item.Display), React.createElement("td", null, Item.UNIT), React.createElement("td", null, Item.COST), React.createElement("td", null, Item.DESC), React.createElement("td", null, Item.IsCentered));
             Rows.push(Test);
         }
-        return (React.createElement("table", {"id": "QuoteDisplay-" + this.props.ID, "className": "table-striped table-bordered"}, React.createElement("tr", null, React.createElement("td", {"colSpan": 5, "style": { "text-align": "center" }}, React.createElement("h3", null, this.props.Name))), Rows));
+        return (React.createElement("table", {"id": "QuoteDisplay-" + this.props.ID, "className": "table-striped table-bordered"}, React.createElement("tbody", null, React.createElement("tr", null, React.createElement("td", {"colSpan": 5, "style": { "text-align": "center" }}, React.createElement("h3", null, this.props.Name))), Rows)));
     };
     return QuoteDisplay;
 })(React.Component);
@@ -50,27 +82,27 @@ var CompanyDisplay = (function (_super) {
                 for (var _i = 0; _i < data.length; _i++) {
                     var Item = data[_i];
                     var Quote = Item;
-                    var T1 = new QuoteDisplay(Quote, _this.context);
                     var Test = React.renderToString(React.createElement(QuoteDisplay, {"ID": Quote.ID, "Name": Quote.Name, "Date": Quote.Date, "Lines": Quote.Lines}));
-                    /*
-                    var s1 = React.renderToString(
-                        <table id={"QuoteDisplay-" + Quote.ID} className = "table-striped table-bordered">
-                            <tr><td colSpan={2} style = {{ "text-align": "center" }}><h3>{Quote.Name}</h3></td></tr>
-                            <tr>
-                                <td>Date</td>
-                                <td>{Quote.Date}</td>
-                            </tr>
-                        </table>
-                    );
-                    */
                     $("#fhhgk75d47").append(Test);
+                }
+            });
+        };
+        this.ViewContacts = function (Current) {
+            $("#CompanyDisplay-" + _this.props.ID).append("<div id='fhhgk75d471' />");
+            var LastQuoteElement = document.getElementById("fhhgk75d47");
+            $.getJSON("/Companies/GetContacts/" + _this.props.ID, function (data) {
+                for (var _i = 0; _i < data.length; _i++) {
+                    var Item = data[_i];
+                    var Contact = Item;
+                    var Test = React.renderToString(React.createElement(ContactDisplay, {"ID": Contact.ID, "Name": Contact.Name, "Email": Contact.Email, "Phone": Contact.Phone, "Position": Contact.Position, "Notes": Contact.Notes}));
+                    $("#fhhgk75d471").append(Test);
                 }
             });
         };
     }
     CompanyDisplay.prototype.render = function () {
         var _this = this;
-        return (React.createElement("table", {"id": "CompanyDisplay-" + this.props.ID, "className": "table-striped table-bordered"}, React.createElement("tr", null, React.createElement("td", {"colSpan": 2, "style": { "text-align": "center" }}, React.createElement("h3", null, this.props.Name))), React.createElement("tr", null, React.createElement("td", null, "ID"), React.createElement("td", null, this.props.ID)), React.createElement("tr", null, React.createElement("td", null, "Address"), React.createElement("td", null, this.props.Address)), React.createElement("tr", null, React.createElement("td", null, "City"), React.createElement("td", null, this.props.City)), React.createElement("tr", null, React.createElement("td", null, "Phone"), React.createElement("td", null, this.props.Phone)), React.createElement("tr", null, React.createElement("td", null, "Zip"), React.createElement("td", null, this.props.Zip)), React.createElement("tr", null, React.createElement("td", null, "Misc"), React.createElement("td", null, this.props.Misc)), React.createElement("tr", null, React.createElement("td", {"colSpan": 2}, React.createElement("a", {"className": "btn btn-primary", "href": "/Companies/Edit/" + this.props.ID}, "Edit"))), React.createElement("tr", null, React.createElement("td", {"colSpan": 2}, React.createElement("a", {"className": "btn btn-primary", "onClick": function () { return _this.ViewQuotes(_this); }}, "View Quotes")))));
+        return (React.createElement("table", {"id": "CompanyDisplay-" + this.props.ID, "className": "table-striped table-bordered"}, React.createElement("tbody", null, React.createElement("tr", null, React.createElement("td", {"colSpan": 2, "style": { "text-align": "center" }}, React.createElement("h3", null, this.props.Name))), React.createElement("tr", null, React.createElement("td", null, "ID"), React.createElement("td", null, this.props.ID)), React.createElement("tr", null, React.createElement("td", null, "Address"), React.createElement("td", null, this.props.Address)), React.createElement("tr", null, React.createElement("td", null, "City"), React.createElement("td", null, this.props.City)), React.createElement("tr", null, React.createElement("td", null, "Phone"), React.createElement("td", null, this.props.Phone)), React.createElement("tr", null, React.createElement("td", null, "Zip"), React.createElement("td", null, this.props.Zip)), React.createElement("tr", null, React.createElement("td", null, "Misc"), React.createElement("td", null, this.props.Misc)), React.createElement("tr", null, React.createElement("td", {"colSpan": 2}, React.createElement("a", {"className": "btn btn-primary", "href": "/Companies/Edit/" + this.props.ID}, "Edit"))), React.createElement("tr", null, React.createElement("td", {"colSpan": 2}, React.createElement("a", {"className": "btn btn-primary", "onClick": function () { return _this.ViewQuotes(_this); }}, "View Quotes"))), React.createElement("tr", null, React.createElement("td", {"colSpan": 2}, React.createElement("a", {"className": "btn btn-primary", "onClick": function () { return _this.ViewContacts(_this); }}, "View Contacts"))))));
     };
     return CompanyDisplay;
 })(React.Component);
