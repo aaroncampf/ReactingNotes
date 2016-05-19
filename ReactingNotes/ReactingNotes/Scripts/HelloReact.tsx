c16df33afb55bf4c71418098628b051f0229a77f
Aaron Campf <aaroncampf@gmail.com>
2016-05-19 12:36:04 -0700

    More Work

--- ReactingNotes/ReactingNotes/Scripts/HelloReact.tsx
+++ ReactingNotes/ReactingNotes/Scripts/HelloReact.tsx
@@ -30,6 +30,76 @@ class QuoteLine {
     public IsCentered : boolean
 }
 
+class Contact {
+    public ID: number
+    public Name: string
+    public Phone: string
+    public Email: string
+    public Position: string
+    public Notes: Note[]
+}
+
+class Note {
+    public ID: number
+    public Date: Date
+    public Title: string
+    public Text: string
+}
+
+
+class ContactDisplay extends React.Component<Contact, Contact> {
+    private ViewNotes = (Current: ContactDisplay) => { //TODO: Get help removing this BS because [this] is broken
+        $("#ContactDisplay-" + this.props.ID).append("<div id='fhhgk75d472' />")
+        var Rows: JSX.Element[] = []
+        for (var Item of this.props.Notes) {
+            var Test =
+                <tr>
+                    <td>{Item.ID}</td>
+                    <td>{Item.Date}</td>
+                    <td>{Item.Title}</td>
+                    <td>{Item.Text}</td>
+                </tr>
+            Rows.push(Test)
+        }
+
+        return (
+            <table id={"QuoteDisplay-" + this.props.ID} className = "table-striped table-bordered">
+                <tbody>
+                    <tr><td colSpan={5} style = {{ "text-align": "center" }}><h3>{this.props.Name}</h3></td></tr>
+                    {Rows}
+                    </tbody>
+            </table>);        
+    }
+
+    render() {
+        return (
+            <table id={"ContactDisplay-" + this.props.ID} className = "table-striped table-bordered">
+                <tbody>
+                    <tr><td colSpan={2} style = {{ "text-align": "center" }}><h3>{this.props.Name}</h3></td></tr>
+                    <tr>
+                        <td>ID</td>
+                        <td>{this.props.ID}</td>
+                    </tr>
+                    <tr>
+                        <td>Phone</td>
+                        <td>{this.props.Phone}</td>
+                    </tr>
+                    <tr>
+                        <td>Email</td>
+                        <td>{this.props.Email}</td>
+                    </tr>
+                    <tr>
+                        <td>Position</td>
+                        <td>{this.props.Position}</td>
+                    </tr>
+                    <tr>
+                        <td colSpan={2}><a className = "btn btn-primary" onClick= {() => this.ViewNotes(this) }>View Contacts</a></td>
+                    </tr> 
+                </tbody>
+            </table>);
+    }
+}
+
 
 
 class QuoteDisplay extends React.Component<Quote, Quote> {
@@ -49,8 +119,10 @@ class QuoteDisplay extends React.Component<Quote, Quote> {
 
         return(
             <table id={"QuoteDisplay-" + this.props.ID} className = "table-striped table-bordered">
-                <tr><td colSpan={5} style = {{ "text-align": "center" }}><h3>{this.props.Name}</h3></td></tr>
-                {Rows}
+                <tbody>
+                    <tr><td colSpan={5} style = {{ "text-align": "center" }}><h3>{this.props.Name}</h3></td></tr>
+                    {Rows}
+                </tbody>
             </table>);
     }
 }
@@ -66,24 +138,28 @@ class CompanyDisplay extends React.Component<Company, Company> {
                 for (var Item of data) {
                     var Quote: Quote = Item
 
-                    var T1 = new QuoteDisplay(Quote, this.context)
                     var Test = React.renderToString(
                         <QuoteDisplay ID = {Quote.ID} Name = {Quote.Name} Date = {Quote.Date} Lines = {Quote.Lines} />
                     );
-
-
-                    /*
-                    var s1 = React.renderToString(
-                        <table id={"QuoteDisplay-" + Quote.ID} className = "table-striped table-bordered">
-                            <tr><td colSpan={2} style = {{ "text-align": "center" }}><h3>{Quote.Name}</h3></td></tr>
-                            <tr>
-                                <td>Date</td>
-                                <td>{Quote.Date}</td>
-                            </tr>   
-                        </table>
-                    );
-                    */
                     $("#fhhgk75d47").append(Test)
+                }
+            }
+        )
+    }
+
+    private ViewContacts = (Current: CompanyDisplay) => { //TODO: Get help removing this BS because [this] is broken
+        $("#CompanyDisplay-" + this.props.ID).append("<div id='fhhgk75d471' />")
+        var LastQuoteElement = document.getElementById("fhhgk75d47")
+
+        $.getJSON("/Companies/GetContacts/" + this.props.ID,
+            (data) => {
+                for (var Item of data) {
+                    var Contact: Contact = Item
+
+                    var Test = React.renderToString(
+                        <ContactDisplay ID = {Contact.ID} Name = {Contact.Name} Email = {Contact.Email} Phone = {Contact.Phone} Position = {Contact.Position} Notes = {Contact.Notes} />
+                    );
+                    $("#fhhgk75d471").append(Test)
                 }
             }
         )
@@ -92,37 +168,42 @@ class CompanyDisplay extends React.Component<Company, Company> {
     render() {
         return (
             <table id={"CompanyDisplay-" + this.props.ID} className = "table-striped table-bordered">
-                <tr><td colSpan={2} style = {{ "text-align": "center" }}><h3>{this.props.Name}</h3></td></tr>
-                <tr>
-                    <td>ID</td>
-                    <td>{this.props.ID}</td>
+                <tbody>
+                    <tr><td colSpan={2} style = {{ "text-align": "center" }}><h3>{this.props.Name}</h3></td></tr>
+                    <tr>
+                        <td>ID</td>
+                        <td>{this.props.ID}</td>
+                        </tr>
+                    <tr>
+                        <td>Address</td>
+                        <td>{this.props.Address}</td>
+                        </tr>
+                    <tr>
+                        <td>City</td>
+                        <td>{this.props.City}</td>
+                        </tr>
+                    <tr>
+                        <td>Phone</td>
+                        <td>{this.props.Phone}</td>
+                        </tr>
+                    <tr>
+                        <td>Zip</td>
+                        <td>{this.props.Zip}</td>
+                        </tr>
+                    <tr>
+                        <td>Misc</td>
+                        <td>{this.props.Misc}</td>
+                        </tr>
+                    <tr>
+                        <td colSpan={2}><a className = "btn btn-primary" href={"/Companies/Edit/" + this.props.ID}>Edit</a></td>
                     </tr>
-                <tr>
-                    <td>Address</td>
-                    <td>{this.props.Address}</td>
-                    </tr>
-                <tr>
-                    <td>City</td>
-                    <td>{this.props.City}</td>
-                    </tr>
-                <tr>
-                    <td>Phone</td>
-                    <td>{this.props.Phone}</td>
-                    </tr>
-                <tr>
-                    <td>Zip</td>
-                    <td>{this.props.Zip}</td>
-                    </tr>
-                <tr>
-                    <td>Misc</td>
-                    <td>{this.props.Misc}</td>
-                    </tr>
-                <tr>
-                    <td colSpan={2}><a className = "btn btn-primary" href={"/Companies/Edit/" + this.props.ID}>Edit</a></td>
-                </tr>
-                <tr>
-                    <td colSpan={2}><a className = "btn btn-primary" onClick= {() => this.ViewQuotes(this) }>View Quotes</a></td>
-                </tr> 
+                    <tr>
+                        <td colSpan={2}><a className = "btn btn-primary" onClick= {() => this.ViewQuotes(this)}>View Quotes</a></td>
+                    </tr> 
+                    <tr>
+                        <td colSpan={2}><a className = "btn btn-primary" onClick= {() => this.ViewContacts(this)}>View Contacts</a></td>
+                    </tr> 
+                </tbody>
             </table>);
     }
 }
